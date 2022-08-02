04c31118c5a280f3837e7821754c791765ee2f5a
qinweijian <qinweijian@lrhealth.com>
2022-08-02 18:13:11 +0800

    Dev

---
+++ src/components/list/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="accordin_card">
+    <div class="title"><span class="icon"></span>{{ title }}</div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+<style lang="scss" scoped>
+.accordin_card {
+  color: #606266;
+  .title {
+    font-size: 13px;
+    .icon {
+      color: #8D8D8D;
+      font-size: 12px;
+      font-family: element-icons !important;
+      display: inline-block;
+      padding: 6px;
+      &::before {
+        content: '\e791';
+      }
+    }
+  }
+  .content{
+      padding-left: 24px;
+  }
+}
+</style>
--- src/utils/lang.js
+++ src/utils/lang.js
@@ -5,3 +5,11 @@ export function keysObject(ar, keyName) {
   })
   return re
 }
+
+export function isEmpty(val) {
+  if (val == null) return true
+  if (typeof val == 'string' && val.trim() == '') {
+    return true
+  }
+  return false
+}
