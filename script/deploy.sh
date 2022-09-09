bb456ffc685854e5b95568ab7a51fb14e484e24b
Wen Bao Yin <supermq@gmail.com>
2022-09-09 12:44:05 +0800

    fix: load 剂型 from value dic

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,10 @@
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 echo $DIR
 cd $DIR/..
+
+hosts=([dev]="192.168.29.32" [beta]="36.133.124.47")
+hostName=$1
+echo ${hosts[$hostName]}
+
 yarn build
-rsync -av -e "ssh -p 29022" ./dist/ root@192.168.29.32:/usr/share/nginx/data-standard
+rsync -av -e "ssh -p 29022" ./dist/ root@${hosts[$hostName]}:/usr/share/nginx/data-standard
