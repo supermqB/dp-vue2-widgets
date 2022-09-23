5a26b186d00d4d5909373856cb874ffead29bc5f
Wen Bao Yin <supermq@gmail.com>
2022-09-23 10:01:27 +0800

    chore: extract common doclist utils

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-// const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const path = require('path')
 
 module.exports = defineConfig({
-  // transpileDependencies: true,
   lintOnSave: false,
   productionSourceMap: process.env.NODE_ENV === 'development',
   configureWebpack: {
-    // target: 'web',
-    devtool: 'source-map',
     resolve: {
       alias: {
         '@': path.resolve('src'),
         '@img': path.resolve('src/assets/images')
       }
-      // fallback: {
-      //   fs: false,
-      //   net: false,
-      //   crypto: require.resolve('crypto-browserify')
-      // }
     }
-    // plugins: [new NodePolyfillPlugin()]
   },
   css: {
     loaderOptions: {
