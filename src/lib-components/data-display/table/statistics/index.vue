702013066b89ae0c3a5b89db6ca63ec749b74b88
unknown <17600827518@163.com>
2022-11-30 16:13:25 +0800

    [fix]: statisticsTable添加border

--- src/lib-components/data-display/table/statistics/index.vue
+++ src/lib-components/data-display/table/statistics/index.vue
@@ -6,7 +6,7 @@
         :data="data"
         style="width: 100%"
         :span-method="spanMethod"
-        stripe
+        border
       >
         <tableColumn v-for="item in tableTitle" :key="item.prop" :item="item" />
       </el-table>
