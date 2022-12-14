16b12cf5ead1e3f62cd2e1fda359f30db2037ea3
qinweijian <qinweijian@lrhealth.com>
2022-12-14 18:57:25 +0800

    sideCatalog 点击emit出ref，溢出样式调整

--- dist/dp-vue2-widgets.ssr.js
+++ dist/dp-vue2-widgets.ssr.js
@@ -1250,6 +1250,7 @@ var script$r = {
           var id = node.id,
             type = node.type,
             label = node.label;
+          var dom = _this2.$refs["dom".concat(id)];
           var ids = id.split('-');
           _this2.$emit('onItemSelected', {
             id: ids.length > 1 ? ids[1] : ids[0],
@@ -1258,8 +1259,8 @@ var script$r = {
             list: list.map(function (item) {
               return item.id;
             }).join('.')
-          });
-          _this2.$emit('onNodeSelected', _objectSpread2({}, node));
+          }, dom);
+          _this2.$emit('onNodeSelected', _objectSpread2({}, node), dom);
         }
       });
     }
@@ -1313,15 +1314,18 @@ var __vue_render__$s = function __vue_render__() {
         var data = ref.data;
         var node = ref.node;
         return _c('div', {
+          ref: 'dom' + data.id,
           staticClass: "treeNode",
           on: {
             "click": function click(event) {
               return _vm.isDisabledByType(data.type) ? event.stopPropagation() : null;
             }
           }
-        }, [_c('p', {
+        }, [_c('div', {
           staticClass: "label"
-        }, [_c('span', [data.state ? _c('i') : _c('span', {
+        }, [_c('span', {
+          staticClass: "front"
+        }, [data.state ? _c('i') : _c('span', {
           staticClass: "blank"
         }), _vm._v(" "), _c('span', [_vm._v(_vm._s(data.label))])]), _vm._v(" "), _c('span', [_c('span', [_vm._v(_vm._s('number' in data ? _vm.unitFmt(data.number) : ''))]), _vm._v(" "), _c('span', [_vm._v(_vm._s(data.endText))])])]), _vm._v(" "), !node.isLeaf ? _c('div', {
           staticClass: "disabled"
@@ -1335,16 +1339,16 @@ var __vue_staticRenderFns__$s = [];
 /* style */
 var __vue_inject_styles__$s = function __vue_inject_styles__(inject) {
   if (!inject) return;
-  inject("data-v-3b68768a_0", {
-    source: ".wrap[data-v-3b68768a]{width:100%;height:100%;overflow-x:hidden;display:flex;flex-direction:column}.wrap .treeWrap[data-v-3b68768a]{flex:1;overflow:auto}.treeNode[data-v-3b68768a]{width:100%;height:100%;padding-top:2px;padding-right:10px;display:flex;box-sizing:border-box;justify-content:space-between;align-items:center;font-size:13px}.treeNode .label[data-v-3b68768a]{width:100%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;display:flex;justify-content:space-between;align-items:center}.treeNode .label i[data-v-3b68768a]{display:inline-block;width:5px;height:5px;margin-right:3px;margin-bottom:2px;border-radius:5px;background-color:#f56c6c}.treeNode .label .blank[data-v-3b68768a]{display:inline-block;margin-right:3px;margin-bottom:2px;width:5px;height:5px}.disabled[data-v-3b68768a]{position:absolute;left:0;width:100%;height:100%;z-index:10}[data-v-3b68768a] .el-tree-node__content{height:36px;position:relative}[data-v-3b68768a] .el-tree-node__content>.el-tree-node__expand-icon{z-index:12;padding:4px;display:inline-block}[data-v-3b68768a] .el-tree-node.is-current>.el-tree-node__content{background-color:#f2f6ff!important}[data-v-3b68768a] .el-tree-node:focus>.el-tree-node__content{background-color:transparent}",
+  inject("data-v-77938cd9_0", {
+    source: ".wrap[data-v-77938cd9]{width:100%;height:100%;overflow-x:hidden;display:flex;flex-direction:column}.wrap .treeWrap[data-v-77938cd9]{flex:1;overflow:auto}.treeNode[data-v-77938cd9]{width:100%;height:100%;padding-top:2px;padding-right:10px;display:flex;box-sizing:border-box;justify-content:space-between;align-items:center;overflow:hidden;font-size:13px}.treeNode .label[data-v-77938cd9]{width:100%;display:flex;justify-content:space-between;align-items:center;overflow:hidden}.treeNode .label .front[data-v-77938cd9]{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.treeNode .label i[data-v-77938cd9]{display:inline-block;width:5px;height:5px;padding-right:3px;padding-bottom:2px;border-radius:5px;background-color:#f56c6c}.treeNode .label .blank[data-v-77938cd9]{display:inline-block;padding-right:3px;padding-bottom:2px;width:5px;height:5px}.disabled[data-v-77938cd9]{position:absolute;left:0;width:100%;height:100%;z-index:10}[data-v-77938cd9] .el-tree-node__content{height:36px;position:relative}[data-v-77938cd9] .el-tree-node__content>.el-tree-node__expand-icon{z-index:12;padding:4px;display:inline-block}[data-v-77938cd9] .el-tree-node.is-current>.el-tree-node__content{background-color:#f2f6ff!important}[data-v-77938cd9] .el-tree-node:focus>.el-tree-node__content{background-color:transparent}",
     map: undefined,
     media: undefined
   });
 };
 /* scoped */
-var __vue_scope_id__$s = "data-v-3b68768a";
+var __vue_scope_id__$s = "data-v-77938cd9";
 /* module identifier */
-var __vue_module_identifier__$s = "data-v-3b68768a";
+var __vue_module_identifier__$s = "data-v-77938cd9";
 /* functional template */
 var __vue_is_functional_template__$s = false;
 /* style inject shadow dom */
